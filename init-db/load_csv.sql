d8c6b0d21f1f0e431b0cf83ba827fd45dc3da78e
vedatenisgul <gulv22@itu.edu.tr>
2025-11-07 21:03:40 +0300

    Created load db data structure and added movies and genres part

---
+++ init-db/load_csv.sql
@@ -0,0 +1,17 @@
+-- Genres
+\copy genres(id, genre_name) 
+FROM '/docker-entrypoint-initdb.d/db_genres.csv' 
+DELIMITER ',' 
+CSV HEADER;
+
+-- Movies 
+\copy movies(id, title, overview, tagline, release_date, poster_url, platform_id) 
+FROM '/docker-entrypoint-initdb.d/db_movies.csv' 
+DELIMITER ',' 
+CSV HEADER;
+
+-- Movies_Genres
+\copy movies_genres(movie_id, genre_id) 
+FROM '/docker-entrypoint-initdb.d/db_movies_genres.csv' 
+DELIMITER ',' 
+CSV HEADER;
