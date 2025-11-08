439eb5bc78b63f97d3a30afc766187416d454727
Vedat Enis Gül <145773536+vedatenisgul@users.noreply.github.com>
2025-11-08 11:07:36 +0300

    fix copy commands in load_csv.sql

--- init-db/load_csv.sql
+++ init-db/load_csv.sql
@@ -1,17 +1,8 @@
 -- Genres
-\copy genres(id, genre_name) 
-FROM '/docker-entrypoint-initdb.d/db_genres.csv' 
-DELIMITER ',' 
-CSV HEADER;
+\copy genres(id, genre_name) FROM '/docker-entrypoint-initdb.d/db_genres.csv' DELIMITER ',' CSV HEADER
 
 -- Movies 
-\copy movies(id, title, overview, tagline, release_date, poster_file, banner_file, platform_id) 
-FROM '/docker-entrypoint-initdb.d/db_movies.csv' 
-DELIMITER ',' 
-CSV HEADER;
+\copy movies(id, title, overview, tagline, release_date, poster_file, banner_file, platform_id) FROM '/docker-entrypoint-initdb.d/db_movies.csv' DELIMITER ',' CSV HEADER
 
 -- Movies_Genres
-\copy movies_genres(movie_id, genre_id) 
-FROM '/docker-entrypoint-initdb.d/db_movies_genres.csv' 
-DELIMITER ',' 
-CSV HEADER;
+\copy movies_genres(movie_id, genre_id) FROM '/docker-entrypoint-initdb.d/db_movies_genres.csv' DELIMITER ',' CSV HEADER
