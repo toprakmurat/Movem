49c734456040ce44639ffcd38108f1eb27f7b911
Vedat Enis Gül <145773536+vedatenisgul@users.noreply.github.com>
2025-11-16 13:39:32 +0300

    Refactor function names

--- src/routes/movies.py
+++ src/routes/movies.py
@@ -1,12 +1,12 @@
 from flask import Blueprint, jsonify
 from src.config.database import execute_query
 from src.services.movie_service import (
-    get_movies,
-    get_movie_by_id,
-    create_movie,
-    update_movie,
-    delete_movie_by_id,
-    get_movies_by_genre_id
+    get_movies_db,
+    get_movie_by_id_db,
+    create_movie_db,
+    update_movie_db,
+    delete_movie_db,
+    get_movies_by_genre_db
 )
 
 movies_bp = Blueprint('movies', __name__)
@@ -16,16 +16,16 @@ movies_bp = Blueprint('movies', __name__)
 def get_movies():
     """Get all movies"""
 
-    movies, err = get_movies()
+    movies, err = get_movies_db()
     if err:
         return jsonify({"error": err}), 500
     return jsonify([dict(movie) for movie in movies]), 200
 
 
 @movies_bp.route('/<int:movie_id>', methods=['GET'])
-def get_movie(movie_id):
+def get_movie_by_id(movie_id):
     """Get a specific movie"""
-    movie, err = get_movie_by_id(movie_id)
+    movie, err = get_movie_by_id_db(movie_id)
     if err:
         return jsonify({"error": err}), 500
     if not movie:
@@ -34,26 +34,26 @@ def get_movie(movie_id):
 
 
 @movies_bp.route("/", methods=["POST"])
-def create_new_movie():
+def create_movie():
     """Get a new movie"""
     movie_data = request.get_json()
     if not movie_data:
         return jsonify({"error": "No data provided"}), 400
 
-    new_movie, err = create_movie(movie_data)
+    new_movie, err = create_movie_db(movie_data)
     if err:
         return jsonify({"error": err}), 400
     return jsonify(dict(new_movie)), 201
 
 
 @movies_bp.route("/<int:id>", methods=["PUT"])
-def update_existing_movie(id):
+def update_movie(id):
     """Update movie by id"""
     movie_data = request.get_json()
     if not movie_data:
         return jsonify({"error": "No data provided"}), 400
 
-    updated_movie, err = update_movie(id, movie_data)
+    updated_movie, err = update_movie_db(id, movie_data)
     if err:
         return jsonify({"error": err}), 400
     if not updated_movie:
@@ -65,7 +65,7 @@ def update_existing_movie(id):
 @movies_bp.route("/<int:id>", methods=["DELETE"])
 def delete_movie(id):
     """Delete movie by id"""
-    deleted_movie, err = delete_movie_by_id(id)
+    deleted_movie, err = delete_movie_db(id)
     if err:
         return jsonify({"error": err}), 500
     if not deleted_movie:
@@ -76,7 +76,7 @@ def delete_movie(id):
 @movies_bp.route("/genre/<int:genre_id>", methods=["GET"])
 def get_movies_by_genre(genre_id):
     """Get movie by genre_id"""
-    movies, err = get_movies_by_genre_id(genre_id)
+    movies, err = get_movies_by_genre_db(genre_id)
     if err:
         return jsonify({"error": err}), 500
     if not movies:
