a1e9d6fadc0f790cadf748f16d63a90665231d2e
Vedat Enis Gül <145773536+vedatenisgul@users.noreply.github.com>
2025-11-21 00:51:58 +0300

    Fix primary key definition in movies and genres tables

--- init-db/init.sql
+++ init-db/init.sql
@@ -39,7 +39,7 @@ CREATE TABLE IF NOT EXISTS platforms (
 -- movies table 
 ------------------------------------------------------------
 CREATE TABLE IF NOT EXISTS movies(
-    id PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
     overview TEXT,
     tagline  VARCHAR(256),
@@ -77,7 +77,7 @@ CREATE TABLE IF NOT EXISTS movie_cast (
 -- genres table
 ------------------------------------------------------------
 CREATE TABLE IF NOT EXISTS genres (
-    id PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
     genre_name VARCHAR(50) UNIQUE NOT NULL
 );
 
